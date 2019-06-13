f0387710698428f69d943072a9fa63c937b0259c
Abrar <Abrar@172.17.108.152>
2019-06-13 16:10:34 -0500

    Package Structure

--- BankingApplication/src/clientSimulator/BankAppMain.java
+++ BankingApplication/src/clientSimulator/BankAppMain.java
@@ -2,8 +2,6 @@ package clientSimulator;
 
 import java.util.Scanner;
 
-import com.springboot.bankapplication.Menu;
-
 public class BankAppMain   {
 
 	public static void main(String[] args) {
--- BankingApplication/src/clientSimulator/Menu.java
+++ BankingApplication/src/clientSimulator/Menu.java
@@ -1,5 +1,5 @@
 
-package com.springboot.bankapplication;
+package clientSimulator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
